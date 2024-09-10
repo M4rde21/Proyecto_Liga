82f083db91f0e002d451cfbd428bf7ec9ad63995
RomanM22 <roman.bj15@gmail.com>
2024-09-10 16:07:56 -0300

    Primer Avance

--- liga_de_estrellas/Liga_estrella/settings.py
+++ liga_de_estrellas/Liga_estrella/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'liga_estrellas',
         'USER': 'root',
-        'PASSWORD': 'm4rde1996',
+        'PASSWORD': 'martinez2001',
         'HOST': 'localhost',  
         'PORT': '3306',  
     }
